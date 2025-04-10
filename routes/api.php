12aa8418260b3d183001d7b315fe97a33fa595d0
Adriano <redmaen19@gmail.com>
2025-04-09 20:53:21 -0500

    feat: :sparkles: Add route for ClienteGController

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ClienteGController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('/clienteG',ClienteGController::class);
