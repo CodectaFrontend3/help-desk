b31177d3b2ffbfac49e41dfcba6c98adfd697df7
Adriano <redmaen19@gmail.com>
2025-04-09 19:52:34 -0500

    feat: :sparkles: Add softwareController controller

---
+++ app/Http/Controllers/SoftwareController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Software;
+use Illuminate\Http\Request;
+
+class SoftwareController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Software::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $software = Software::create($request->all());
+        return response()->json($software, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $software = Software::findOrFail($id);
+        return response()->json($software);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $software = Software::findOrFail($id);
+        $software->update($request->all());
+        return response()->json($software, 204);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $software = Software::findOrFail($id);
+        $software->delete();
+        return response()->noContent();
+    }
+}
