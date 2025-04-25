96e89de0e638c1e780f0b4d6bd0c5bd3dfa2a5e1
JhoanR11 <reyesjhoan52119@gmail.com>
2025-04-25 12:31:27 -0500

    Modificación de los endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,20 @@
 <?php
 
+use App\Http\Controllers\BranchController;
 use App\Http\Controllers\MicroCompanyController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ClienteGController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\NaturalPersonController;
 use App\Http\Controllers\RegistroHardwareController;
 use App\Http\Controllers\SoftwareController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/micro-company', [MicroCompanyController::class, "index"]);
-Route::get('/micro-company/{id}', [MicroCompanyController::class, "show"]);
-Route::post('/micro-company', [MicroCompanyController::class, "store"]);
-Route::put('/micro-company/{id}', [MicroCompanyController::class, "update"]);
-Route::delete('/micro-company/{id}', [MicroCompanyController::class, "destroy"]);
-
-Route::get('/natural-person', [NaturalPersonController::class, "index"]);
-Route::get('/natural-person/{id}', [NaturalPersonController::class, "show"]);
-Route::post('/natural-person', [NaturalPersonController::class, "store"]);
-Route::put('/natural-person/{id}', [NaturalPersonController::class, "update"]);
-Route::delete('/natural-person/{id}', [NaturalPersonController::class, "destroy"]);
+Route::apiResource('/micro-company', MicroCompanyController::class);
+Route::apiResource('/natural-person', NaturalPersonController::class);
+Route::apiResource('/company', CompanyController::class);
+Route::apiResource('/branch', BranchController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
