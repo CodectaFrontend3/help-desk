f14c1f004cd261bd1e44ae74f76001e4feb1225e
Adriano <redmaen19@gmail.com>
2025-04-25 09:55:10 -0500

    test: :white_check_mark: Update EquipoTest test

--- tests/Feature/EquipoTest.php
+++ tests/Feature/EquipoTest.php
@@ -18,6 +18,78 @@ it('lista todos los equipos', function () {
     $response->assertJsonCount(3);
 });
 
+it('un clienteG puede tener varios Equipos',function(){
+    $cliente = ClienteG::factory()->create();
+
+    $equipo1= Equipo::factory()->create(['id_clienteG' => $cliente->id]);
+    $equipo2= Equipo::factory()->create(['id_clienteG' => $cliente->id]);
+
+    $cliente->load('equipo');
+
+    $this->assertCount(2,$cliente->equipo);
+
+    $this->assertTrue($cliente->equipo->contains($equipo1));
+    $this->assertTrue($cliente->equipo->contains($equipo2));
+});
+
+it('una empresa puede tener varios Equipos',function(){
+    $empresa = Empresa::factory()->create();
+
+    $equipo1= Equipo::factory()->create(['id_empresa' => $empresa->id]);
+    $equipo2= Equipo::factory()->create(['id_empresa' => $empresa->id]);
+
+    $empresa->load('equipo');
+
+    $this->assertCount(2,$empresa->equipo);
+
+    $this->assertTrue($empresa->equipo->contains($equipo1));
+    $this->assertTrue($empresa->equipo->contains($equipo2));
+});
+
+it('una microempresa puede tener varios Equipos',function(){
+    $microempresa = MicroEmpresa::factory()->create();
+
+    $equipo1= Equipo::factory()->create(['id_microempresa' => $microempresa->id]);
+    $equipo2= Equipo::factory()->create(['id_microempresa' => $microempresa->id]);
+
+    $microempresa->load('equipo');
+
+    $this->assertCount(2,$microempresa->equipo);
+
+    $this->assertTrue($microempresa->equipo->contains($equipo1));
+    $this->assertTrue($microempresa->equipo->contains($equipo2));
+});
+
+it('una personaNatural puede tener varios Equipos',function(){
+    $personaN = PersonaNatural::factory()->create();
+
+    $equipo1= Equipo::factory()->create(['id_personaN' => $personaN->id]);
+    $equipo2= Equipo::factory()->create(['id_personaN' => $personaN->id]);
+
+    $personaN->load('equipo');
+
+    $this->assertCount(2,$personaN->equipo);
+
+    $this->assertTrue($personaN->equipo->contains($equipo1));
+    $this->assertTrue($personaN->equipo->contains($equipo2));
+});
+
+it('un plan puede tener varios Equipos',function(){
+    $plan = Plan::factory()->create();
+
+    $equipo1= Equipo::factory()->create(['id_plan' => $plan->id]);
+    $equipo2= Equipo::factory()->create(['id_plan' => $plan->id]);
+
+    $plan->load('equipo');
+
+    $this->assertCount(2,$plan->equipo);
+
+    $this->assertTrue($plan->equipo->contains($equipo1));
+    $this->assertTrue($plan->equipo->contains($equipo2));
+});
+
+
+
 it('crear un nuevo equipo', function () {
     $cliente = ClienteG::factory()->create();
     $empresa = Empresa::factory()->create();
