4e576ed807eb1ec9193478cb702e9f68afc8d81f
Anthony Condori <alessandro_condori_2005@hotmail.com>
2025-04-30 11:36:22 -0500

    feat: :sparkles: Create request, model, migration, controller, factory, and test for Companies
    - Create 'Company' model. - Create 'CompanyRequest' for request validation. - Implement migration for 'companies' table. - Update 'CompanyController' to use 'CompanyRequest'. - Add tests for show, creating, updating, and deleting companies.

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Permite crear factories para modelos
 
 class Company extends Model
-{
-    protected $table = 'company'; // Especifica el nombre de la tabla en singular
+{   
+    use HasFactory; // Permite crear factories para modelos
     protected $fillable = [
         "client_name",
         "ruc",
