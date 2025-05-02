9ed4c91c10900c72f8397f0f33b11db78d843f5e
Adriano <redmaen19@gmail.com>
2025-05-02 11:47:20 -0500

    feat: :sparkles: Add AcountWorkerController and SoftwareTeamController controller

---
+++ app/Http/Requests/SoftwareTeamRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SoftwareTeamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id_software' => 'required|integer|exists:softwares,id',
+            'id_team' => 'required|integer|exists:teams,id'
+        ];
+    }
+}
