7cbacc691caf49f546c7a491f9b294c1e343fd9a
Adriano <redmaen19@gmail.com>
2025-05-01 21:20:35 -0500

    feat: :building_construction: Create migrations for teams and hardwares

--- database/migrations/2025_04_27_215544_create_clients_g_table.php
+++ database/migrations/2025_04_27_215544_create_clients_g_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('client_g');
+        Schema::dropIfExists('clients_g');
     }
 };
